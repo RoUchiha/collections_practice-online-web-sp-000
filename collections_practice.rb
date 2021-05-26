03832ca7ac67b9e5aba4a30d2475d51063152bb0
Roshaan Singh <swaggsikh@gmail.com>
2021-05-26 23:18:07 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,30 @@
+def sort_array_asc(array)
+  return array.sort
+end
 
+def sort_array_desc(array)
+  return array.sort.reverse
+end
+
+def sort_array_char_count(array)
+ return array.sort do |a, b|
+    a.length <=> b.length
+  end
+end 
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  return array
+end
+
+def reverse_array(array)
+  return array.reverse
+end
+
+def kesha_maker(array)
+  new_array = []
+  array.each do |word|
+    new_array << (word[2] = "$")
+  end
+  return new_array
+end
